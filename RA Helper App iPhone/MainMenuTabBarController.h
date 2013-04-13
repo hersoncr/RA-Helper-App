626dcbcc584735f3d32ff8e38c4a72f721282353
Herson Alfaro <hersonalfaro@macmini-114de1.dormnet.harding.edu>
2013-04-12 22:46:43 -0500

    added tab bar controller

---
+++ RA Helper App iPhone/MainMenuTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuTabBarController.h
+//  RA Helper App iPhone
+//
+//  Created by Herson Alfaro on 4/12/13.
+//  Copyright (c) 2013 Harding University CS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuTabBarController : UITabBarController
+
+@end
